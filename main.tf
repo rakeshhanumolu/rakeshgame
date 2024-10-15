af3543b4573ad1cd52e6a7789c0031fc18940665
rakeshhanumolu <153166473+rakeshhanumolu@users.noreply.github.com>
2024-10-15 12:59:19 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,95 @@
+provider "aws" {
+  region = "us-east-1"  # Set your preferred AWS region
+}
+
+resource "aws_eks_cluster" "my_cluster" {
+  name     = "my-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = "1.28"
+
+  vpc_config {
+    subnet_ids = ["subnet-04b666e86c1623f57", "subnet-0a4317628ed7a618b"]
+  }
+}
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "myAmazonEKSClusterRoles1"
+  
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "eks.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_role_attachment" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# Define the EKS Node Group
+resource "aws_eks_node_group" "my_node_group" {
+  cluster_name    = aws_eks_cluster.my_cluster.name
+  node_group_name = "my-node-group"
+  node_role_arn   = aws_iam_role.eks_node_group_role.arn
+  subnet_ids      = ["subnet-04b666e86c1623f57", "subnet-0a4317628ed7a618b"]
+  instance_types  = ["t3.medium"]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+}
+
+resource "aws_iam_role" "eks_node_group_role" {
+  name = "eks-node-group-role"
+  
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_worker_policy_attachment" {
+  role       = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_cni_policy_attachment" {
+  role       = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_registry_read_only_policy_attachment" {
+  role       = aws_iam_role.eks_node_group_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# Outputs
+output "cluster_name" {
+  value = aws_eks_cluster.my_cluster.name
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.my_cluster.endpoint
+}
+
+output "cluster_certificate_authority" {
+  value = aws_eks_cluster.my_cluster.certificate_authority[0].data
+}
